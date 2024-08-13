24de71f216887971ee9d80fb7a6cfcd994872617
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2024-08-13 15:00:30 -0600

    Adding the first solution problem of Hacker Rank with Java

---
+++ StdinStoud/Solution/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //String myString = scan.next();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+
+        System.out.println(a + "\n" + b + "\n" + c);
+
+        //System.out.println("myString is: "+ myString);
+        //System.out.println("myInt is: "+ a);
+    }
+}
