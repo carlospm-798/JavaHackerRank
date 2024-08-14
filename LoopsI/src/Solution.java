9fd89fc9ca78ac9a59bada373387080261754e5a
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2024-08-13 20:10:11 -0600

    5th problem of Hacker Rank with Java

---
+++ LoopsI/src/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i=1; i<11; i++){
+            System.out.println(N+ " x "+ i+ " = "+ N*i);
+        }
+
+        bufferedReader.close();
+    }
+}
